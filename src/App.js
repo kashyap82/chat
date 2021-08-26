7987ed1bb2cdbf46a44824a79e831b4d7f1331df
vikrant-gloify <vikrant@gloify.com>
2021-08-26 08:21:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import WhatsAppImg from './whatsapp.png';
 import './App.css';
 
 function App() {
+  const [mobile, setMobile] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+          <div className="text-center">
+            <img src={WhatsAppImg} alt="WhatsAppImg" className="w-75 p-2" />
+          </div>
+          <div className="card p-3 mt-1">
+            <div className="card p-3">
+              <div className="form-group">
+                <label className="mb-1" for="mobile">Mobile Number</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  id="mobile"
+                  placeholder="Enter mobile with country code"
+                  onChange={(e) => setMobile(e.target.value)}
+                />
+                <small className="text-muted font-weight-lighter">
+                  Format: 91##########
+                </small>
+              </div>
+              <a className="btn btn-success mt-4" target="_blank" href={`http://wa.me/${mobile}`} rel="noreferrer">
+                Chat on WhatsApp
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
